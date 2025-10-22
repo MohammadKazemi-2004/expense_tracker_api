e5bca16f2691635e73af1df1edcc619fbca33215
mohammad kazemi <kmk.raven403@gmail.com>
2025-10-22 11:15:03 +0330

    add pagination

--- expense_traker/settings.py
+++ expense_traker/settings.py
@@ -147,7 +147,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'dj_rest_auth.jwt_auth.JWTCookieAuthentication',
-    )
+    ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
 }
 
 REST_AUTH = {
