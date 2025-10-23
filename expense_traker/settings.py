cd8566c5ce98e43b63b18acc96b1469b83a6cb16
mohammad kazemi <kmk.raven403@gmail.com>
2025-10-23 11:46:13 +0330

    dockerize

--- expense_traker/settings.py
+++ expense_traker/settings.py
@@ -99,11 +99,11 @@ WSGI_APPLICATION = 'expense_traker.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': '1230103',
-        'HOST':'localhost',
-        'PORT': '5432',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST', 'db'),
+        'PORT': os.environ.get('DB_PORT', '5432'),
     }
 }
 
